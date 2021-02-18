a83659909db53bef5576d40edd06b755912bd706
Anam Munawar <35180489+Anam15@users.noreply.github.com>
2021-02-17 20:25:03 -0500

    Add files via upload

---
+++ components/MyNetwork.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MyNetwork = () => {
+    return(
+        <div>
+            MyNetwork
+        </div>
+    )
+}
+export default MyNetwork
---
+++ components/SignIn.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const SignIn = ()=>{
+    return(
+        <div className = "mycard">
+            <div className="card auth-card input-field">
+                <h2>Connect.io</h2>
+                <input
+                type ="text"
+                placeholder = "email"
+                />
+                <input
+                type ="text"
+                placeholder = "password"
+                />
+                  <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                   <Link to = "/Profile"> Login</Link> 
+                  </button>
+                  <h5>
+                    <Link to ="/SignUp"> Don't have an account?</Link>
+                </h5>
+
+        </div>
+        </div>
+    )
+}
+export default SignIn
