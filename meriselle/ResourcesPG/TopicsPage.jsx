16fcd221fc99707a631a3ca01026e9834694b9ce
meriselle <48763261+meriselleb@users.noreply.github.com>
2021-02-17 21:58:11 -0500

    Add files via upload

---
+++ meriselle/ResourcesPG/TopicsPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link } from "react-router-dom";
+
+class TopicsPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          This is the Topics Page.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default TopicsPage;
---
+++ meriselle/ResourcesPG/tempInbox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link } from "react-router-dom";
+
+class tempinbox extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          This is the inbox page.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default tempinbox;
