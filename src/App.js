f3eeef38e9854b65454342b6718d6b11acc57d7e
Anam Munawar <35180489+Anam15@users.noreply.github.com>
2021-05-14 18:55:29 -0400

    Add files via upload
    This is the src file for the jobboard

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import {NavLink, Route} from 'react-router-dom';
-import './App.css'
-import Home from './components/Home'
-import Messages from './components/Messages'
-import Navigation from './components/Navigation'
-import MyNetwork from './components/MyNetwork'
-import Notifications from './components/Notifications'
-import Resources from './components/Resources'
-import Profile from './components/Profile'
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
-
-
-
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return (
-    <div className="App">
-      
-      <switch>
-        <Route exact path = "/" component = {Home}/>
-        <Route path = "/Messages" component = {Messages}/>
-        <Route path = "/MyNetwork" component = {MyNetwork}/>
-        <Route path = "/Notifications" component = {Notifications}/>
-        <Route path = "/Resources" component = {Resources}/>
-        <Route path = "/Profile" component = {Profile}/>
-        <Route path = "/SignIn" component = {SignIn}/>
-        <Route path = "/SignUp" component = {SignUp}/>
-      </switch>
-    </div>
-  );
+    <Container className="my-4">
+      <h1 className="mb-4">Job Board</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+    </Container>
+  )
 }
 
 export default App;
